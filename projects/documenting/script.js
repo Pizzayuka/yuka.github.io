709965c48cd20f4d8d33fd78a8220872a682f43e
yuka <90258089+Pizzayuka@users.noreply.github.com>
2022-03-27 17:33:04 -0400

    documenting

---
+++ projects/documenting/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("button").mouseover(function(){
+        $("#discription").css("display", "block");
+      });
+      $("#arrow1").mouseout(function(){
+        $("#link1").css("display", "none");
+      });
+});
