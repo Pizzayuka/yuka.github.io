6a91ab21a579c0b88da8e234c082c936268f0081
yuka <90258089+Pizzayuka@users.noreply.github.com>
2022-09-20 16:58:50 +0000

    idk

--- projects/input-output/script2.js
+++ projects/input-output/script2.js
@@ -7,21 +7,22 @@ let imageHolder = document.createElement("div");
 imageHolder.classList.add("origami");
 wrapper.appendChild(imageHolder);
 
-i = 0;
-
-var imgArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
-
-imageHolder.style.backgroundImage = "url(assets/crane-" + imgArray[i] + ".png)";
+i = 1;
 
 
+// var imgArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+
+imageHolder.style.backgroundImage = "url(assets/crane-" + i + ".png)";
 
 document.addEventListener("keydown", 
     function(event){
         console.log("key down");
-        imageHolder.style.backgroundImage = "url(assets/crane-" + imgArray[i++] + ".png)";
-      
-}
-
+        console.log(i);
+        if (i < 24) {
+            imageHolder.style.backgroundImage = "url(assets/crane-" + Number(1 + i) + ".png)";
+            i++;
+        } 
+    }
 ); 
 
 
