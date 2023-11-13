526eb1dc8489f93e3f17eb95d4946740ca138b69
yuka <90258089+Pizzayuka@users.noreply.github.com>
2023-11-13 10:34:13 -0500

    idk

--- projects/documenting/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("button").mouseover(function(){
-        $("#discription").css("display", "block");
-      });
-      $("#arrow1").mouseout(function(){
-        $("#link1").css("display", "none");
-      });
-});
